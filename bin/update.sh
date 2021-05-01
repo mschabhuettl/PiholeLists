c60322632697ae4a003694a7e62669d0febb0d04
Matthias Schabhüttl <mat.s.schabhuettl@gmail.com>
2021-05-01 14:14:36 +0200

    added update.sh file for easy (full) update

---
+++ bin/update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# check for root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# update system's package list
+apt update
+
+# upgrade all installed packages
+apt full-upgrade -y
+
+# update Pi-hole Core, Web Interface and FTL
+pihole -up
+
+# disable default gravity update schedule
+sed -e '/pihole updateGravity/ s/^#*/#/' -i /etc/cron.d/pihole
+
+# update pihole-updatelists script
+pihole-updatelists --update
+
+# wipe all adlists and domains
+sqlite3 /etc/pihole/gravity.db "DELETE FROM adlist"
+sqlite3 /etc/pihole/gravity.db "DELETE FROM adlist_by_group"
+sqlite3 /etc/pihole/gravity.db "DELETE FROM domainlist"
+sqlite3 /etc/pihole/gravity.db "DELETE FROM domainlist_by_group"
+
+# update Pi-hole's lists from remote sources
+pihole-updatelists
+
+# update PiVPN
+pivpn update
+
+# reboot Raspberry Pi
+reboot
