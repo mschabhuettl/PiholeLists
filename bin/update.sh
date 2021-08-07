95c234b1013bf820fdeebd97cf33ef098c0d3d2f
Matthias Schabhüttl <63157170+mschabhuettl@users.noreply.github.com>
2021-08-07 13:40:11 +0200

    removed pivpn as currently not in use

--- bin/update.sh
+++ bin/update.sh
@@ -30,8 +30,5 @@ sqlite3 /etc/pihole/gravity.db "DELETE FROM domainlist_by_group"
 # update Pi-hole's lists from remote sources
 pihole-updatelists
 
-# update PiVPN
-pivpn update
-
 # reboot Raspberry Pi
 reboot
